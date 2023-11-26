332c2745bf5e82e8b08271703a39437b693a8f40
Vincent Buchholz <vbu@miracle.dk>
2023-11-26 15:58:22 +0100

    refactor

--- src/main/java/dk/vv/mtogo/order/msvc/facades/OrderFacade.java
+++ src/main/java/dk/vv/mtogo/order/msvc/facades/OrderFacade.java
@@ -6,10 +6,14 @@ import dk.vv.mtogo.order.msvc.enums.OrderStatus;
 import dk.vv.mtogo.order.msvc.pojos.Order;
 import dk.vv.mtogo.order.msvc.pojos.OrderLine;
 import dk.vv.mtogo.order.msvc.repositories.OrderRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+@ApplicationScoped
 public class OrderFacade {
 
     private final OrderRepository orderRepository;
@@ -41,7 +45,6 @@ public class OrderFacade {
             }
         }
 
-
         // calculate order prices
         order.createSubTotal();
         order.createTotal();
@@ -57,27 +60,6 @@ public class OrderFacade {
     public void handleStatusUpdate(int id, int status){
         orderRepository.update("statusId = ?1 where id= ?2",status,id);
     }
-
-
-    //Order
-    //    private int customerId;
-    //    private String comment;
-
-    //
-    //    private int statusId;
-    //
-    //    private BigDecimal subTotal;
-    //
-    //    private BigDecimal total;
-    //
-    //    private int addressId;
-    //
-    //    private int supplierId;
-
-    //ORDERLINES
-    //    private int productId;
-    //    private int quantity;
-
 
 
 }
